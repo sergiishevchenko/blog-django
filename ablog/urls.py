db09fcd51ca800a257ca91b04fd74f6efbccd291
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2020-11-21 20:30:02 +0100

    django user authentication

--- ablog/urls.py
+++ ablog/urls.py
@@ -4,4 +4,6 @@ from django.contrib import admin
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('theblog.urls')),
+    path('members/', include('django.contrib.auth.urls')),
+    path('members/', include('members.urls')),
 ]
--- theblog/forms.py
+++ theblog/forms.py
@@ -16,11 +16,11 @@ class PostForm(forms.ModelForm):
 class EditForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'author', 'body')
+        fields = ('title', 'title_tag', 'author', 'body')
 
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
-            # 'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
+            'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
             'author': forms.Select(attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
         }
