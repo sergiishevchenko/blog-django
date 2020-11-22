a1bd20f5e163017fc4907eba27287ed20e73aab6
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2020-11-22 10:09:06 +0100

    django user authentication

--- theblog/forms.py
+++ theblog/forms.py
@@ -1,26 +1,31 @@
 from django import forms
-from .models import Post
+from .models import Post, Category
+
+# choices = [('coding', 'coding'), ('sports', 'sports'), ('entertainment', 'entertainment')]
+choices = Category.objects.all().values_list('name', 'name')
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'title_tag', 'author', 'body')
+        fields = ('title', 'title_tag', 'author', 'category', 'body')
 
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
             'author': forms.Select(attrs={'class': 'form-control'}),
+            'category': forms.Select(choices=choices, attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
         }
 
 class EditForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'title_tag', 'author', 'body')
+        fields = ('title', 'title_tag', 'author', 'category', 'body')
 
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
             'author': forms.Select(attrs={'class': 'form-control'}),
+            'category': forms.Select(choices=choices, attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
         }
--- theblog/views.py
+++ theblog/views.py
@@ -1,11 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import Post
+from .models import Post, Category
 from .forms import PostForm, EditForm
 from django.urls import reverse_lazy
-
-# def home(request):
-#     return render(request, 'home.html', {})
 
 class HomeView(ListView):
     model = Post
@@ -21,6 +18,12 @@ class AddPostView(CreateView):
     form_class = PostForm
     template_name = 'add_post.html'
     # fields = '__all__'
+
+class AddCategoryView(CreateView):
+    model = Category
+    # form_class = PostForm
+    template_name = 'add_category.html'
+    fields = '__all__'
 
 class UpdatePostView(UpdateView):
     model = Post
