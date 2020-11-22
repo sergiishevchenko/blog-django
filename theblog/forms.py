254f3f5671422cb67a52e469c24d28c52b89dcf8
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2020-11-22 10:55:24 +0100

    fixed db

--- theblog/forms.py
+++ theblog/forms.py
@@ -20,12 +20,11 @@ class PostForm(forms.ModelForm):
 class EditForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'title_tag', 'author', 'category', 'body')
+        fields = ('title', 'title_tag', 'author', 'body')
 
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
             'author': forms.Select(attrs={'class': 'form-control'}),
-            'category': forms.Select(choices=choices, attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
         }
