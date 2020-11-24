bbf2c4195fa747d561580d308565b350723b84bb
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2020-11-24 18:27:51 +0100

    first commit

--- ablog/settings.py
+++ ablog/settings.py
@@ -1,9 +1,8 @@
 import os
-from config.py import SECRET_KEY
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
