a380b370f9fbc9fa5db9e7bbc7e81cca81bc27d7
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2020-11-24 12:36:25 +0100

    comment form

--- theblog/forms.py
+++ theblog/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Post, Category
+from .models import Post, Category, Comment
 
 choices = Category.objects.all().values_list('name', 'name')
 
@@ -29,4 +29,14 @@ class EditForm(forms.ModelForm):
             'category': forms.Select(choices=choices, attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
             'snippet': forms.Textarea(attrs={'class': 'form-control'}),
+        }
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('name', 'body')
+
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
         }
