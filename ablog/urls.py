a86a29df106498d8d4f07406b83f7159ae52c15e
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2020-11-24 18:16:45 +0100

    first commit

--- ablog/urls.py
+++ ablog/urls.py
@@ -8,4 +8,4 @@ urlpatterns = [
     path('', include('theblog.urls')),
     path('members/', include('django.contrib.auth.urls')),
     path('members/', include('members.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
